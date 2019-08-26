663d14adb5161b50806c9397f46315d36d809e71
summer <592234752@qq.com>
2019-08-26 15:58:58 +0800

    更改

--- src/components/CmtItem2.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import cssobj from '@/css/cmtList.scss'
-
-export default function CmtItem(props) {
-    return <div className ={cssobj.item}>
-                <h1 className={cssobj.user}>评论人:{props.user}</h1>
-                <p className={cssobj.content}>评论内容:{props.content}</p>
-             </div>
-}
--- src/components/Hello.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-export default function Hello(props) {
-    return <div>这是一个Hello组件----{props.name}</div> 
-}
